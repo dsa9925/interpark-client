e010a985dd28783ad22b2732e81428f0be00bf23
kimdohyun <dev.dsa9925@gmail.com>
2023-11-08 17:40:09 +0900

    feat : 배포 테스트

--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -1,4 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+
+import { BtCate } from "../components/ui/buttons";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { Navigation } from "swiper/modules";
@@ -8,27 +10,29 @@ import "swiper/css/navigation";
 import "../styles/recommend.css";
 import "../styles/common.css";
 import { useEffect, useRef, useState } from "react";
-
 import axios from "axios";
+import styled from "@emotion/styled";
+import { InnerArea, SectionTag } from "./layout/layout";
 
 function Recommend() {
   // js 코드 자리
   // JSX 의 요소를 React 에서 참조
   const swiperRef = useRef();
-
   // JSON 데이터 저장해 두고, 자료가 바뀌면 화면을 변경할
   // 리액트 변수를 만든다.
   const [htmlTag, setHtmlTag] = useState([]);
 
-  //  외부 데이터 연동 (axios 활용)
+  // 외부 데이터 연동하기 (axios 이용)
   const axiosJsonData = () => {
     axios
       .get("recommend.json")
       .then(function (res) {
         console.log(res.data);
+
+        const result = res.data;
         let arr = [];
-        for (let i = 0; i < res.data.total; i++) {
-          const obj = res.data["good_" + (i + 1)];
+        for (let i = 0; i < result.total; i++) {
+          const obj = result["good_" + (i + 1)];
           arr[i] = obj;
         }
         console.log(arr);
@@ -66,19 +70,25 @@ function Recommend() {
       });
   };
 
+  // html 이 준비가 되면, json 을 불러들이겠다.
+  // 1. 외부데이터 부르기 좋은 자리
+  // 2. html 태그 참조 (useRef 할때 )
+  // 3. window 참조할때
+  // 4. window.addEventListener("scroll"...)
+  // 5. cleanUp 할때 : 컴포넌트 화면에서 사라질때 실행할 함수
+  // 6. 타이머 만들고, 제거할때.
+  // 컴포넌트가 화면에 보여질 때 실행할 내용 기재 장소
+  // use 는 Hook 이라고 합니다.
+  // 원하는 시점을 감시하고 실행할 함수
   useEffect(() => {
-    // 랜더링 될때
-    //  recommend.json 데이터 불러들이기 기능실행
+    // 외부 데이터 불러들이기
     axiosJsonData();
     // getJsonData();
-    return () => {
-      // 삭제될때 (Clean Up 함수)
-    };
   }, []);
 
   return (
-    <section className="recommend">
-      <div className="recommend-inner">
+    <SectionTag pt={0} pb={90}>
+      <InnerArea>
         <div className="recommend-header">
           <h2 className="recommend-title">쇼핑추천</h2>
           <span className="recommend-txt">
@@ -90,18 +100,16 @@ function Recommend() {
           <div className="recommend-category">
             <ul className="recommend-list">
               <li>
-                <button className="recommend-cate-bt recommend-cate-bt-active">
-                  쎈딜
-                </button>
+                <BtCate active={true}>쎈딜</BtCate>
               </li>
               <li>
-                <button className="recommend-cate-bt">베스트</button>
+                <BtCate>베스트</BtCate>
               </li>
               <li>
-                <button className="recommend-cate-bt">블프데이</button>
+                <BtCate>블프데이</BtCate>
               </li>
               <li>
-                <button className="recommend-cate-bt">디지털프라자</button>
+                <BtCate>디지털프라자</BtCate>
               </li>
               <li>
                 <a href="#" className="recommend-cate-bt">
@@ -110,7 +118,6 @@ function Recommend() {
               </li>
             </ul>
           </div>
-
           <div className="recommend-slide-wrap">
             <Swiper
               slidesPerView={4}
@@ -170,8 +177,8 @@ function Recommend() {
         </div>
 
         <div className="recommend-footer"></div>
-      </div>
-    </section>
+      </InnerArea>
+    </SectionTag>
   );
 }
 export default Recommend;
