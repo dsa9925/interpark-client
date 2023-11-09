8f29f8c34bce47569ae0f438e793eda42b6d9242
kimdohyun <dev.dsa9925@gmail.com>
2023-11-09 14:55:14 +0900

    feat: Header.js emotion 적용

---
+++ src/styled/header.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const HeaderTopLeft = styled.div`
+  position: relative;
+  width: ${(props) => props.w + "px"};
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
